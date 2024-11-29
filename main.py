a5053f88f21670ec3c7ab8c850cde767939b52d2
EnnaLea <soleariaeve@gmail.com>
2024-11-29 16:24:46 +0100

    end of the code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import time
+import glob
+import os
 from emailing import send_email
+from threading import  Thread
 
 # start a video
 # the number refer to numbers of cameras, in a laptop with
@@ -10,13 +13,19 @@ video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
-
 status_list = []
+count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 # show the image
 while True:
     status = 0
     check, frame = video.read()
+
     # convert the frames in grayscale
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # blur the frame
@@ -31,7 +40,7 @@ while True:
     # classify the picture
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    #cv2.imshow("My video", dil_frame)
+    cv2.imshow("My video", dil_frame)
 
     # find contours
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -43,6 +52,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
 
     status_list.append(status)
@@ -50,7 +64,13 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+
 
     print(status_list)
 
@@ -62,4 +82,6 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
 
